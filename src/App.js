de802e332674ea5ee788ddfe53be03c74bc5cffb
webdev.sp <webdev.sp@amtexsystems.com>
2023-08-21 15:29:47 +0530

    added restore and eraser size

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
      <ImageEditor/>
+     <img src="/newTest.png" alt="test"/>
     </div>
   );
 }
--- src/ImageEditor.jsx
+++ src/ImageEditor.jsx
@@ -1,106 +1,3 @@
-// import React, { useState, useRef } from 'react';
-
-// function ImageEditor() {
-//   const canvasRef = useRef(null);
-//   const [ctx, setCtx] = useState(null);
-//   const [history, setHistory] = useState([]);
-//   const [historyIndex, setHistoryIndex] = useState(-1);
-//   const [image, setImage] = useState(null);
-
-//   const handleUploadImage = (event) => {
-//     const file = event.target.files[0];
-//     if (file) {
-//       const reader = new FileReader();
-//       reader.onload = (e) => {
-//         const newImage = new Image();
-//         newImage.src = e.target.result;
-//         newImage.onload = () => {
-//           setImage(newImage);
-//           const canvas = canvasRef.current;
-//           const newCtx = canvas.getContext('2d');
-//           newCtx.drawImage(newImage, 0, 0, canvas.width, canvas.height);
-//           setCtx(newCtx);
-//           setHistory([{ imageData: newCtx.getImageData(0, 0, canvas.width, canvas.height) }]);
-//           setHistoryIndex(0);
-//         };
-//       };
-//       reader.readAsDataURL(file);
-//     }
-//   };
-
-//   const handleEraser = () => {
-//     if (ctx) {
-//       const canvas = canvasRef.current;
-//       canvas.addEventListener('mousemove', erase);
-//       canvas.addEventListener('mouseup', stopErasing);
-//     }
-//   };
-
-//   const erase = (e) => {
-//     const canvas = canvasRef.current;
-//     const canvasRect = canvas.getBoundingClientRect();
-//     const x = e.clientX - canvasRect.left;
-//     const y = e.clientY - canvasRect.top;
-
-//     ctx.clearRect(x, y, 20, 20);
-//     ctx.fillStyle = 'rgba(0, 0, 0, 0)'; // Transparent color
-//     ctx.fillRect(x, y, 20, 20);
-//   };
-
-//   const stopErasing = () => {
-//     const canvas = canvasRef.current;
-//     canvas.removeEventListener('mousemove', erase);
-//     saveState();
-//   };
-
-//   const undo = () => {
-//     if (historyIndex > 0) {
-//       setHistoryIndex(historyIndex - 1);
-//       const previousState = history[historyIndex - 1];
-//       ctx.putImageData(previousState.imageData, 0, 0);
-//     }
-//   };
-
-//   const redo = () => {
-//     if (historyIndex < history.length - 1) {
-//       setHistoryIndex(historyIndex + 1);
-//       const nextState = history[historyIndex + 1];
-//       ctx.putImageData(nextState.imageData, 0, 0);
-//     }
-//   };
-
-//   const saveState = () => {
-//     const canvas = canvasRef.current;
-//     const newImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-//     setHistory([...history.slice(0, historyIndex + 1), { imageData: newImageData }]);
-//     setHistoryIndex(historyIndex + 1);
-//   };
-
-//   const saveImage = () => {
-//     if (ctx) {
-//       const canvas = canvasRef.current;
-//       const editedImageURL = canvas.toDataURL('image/png');
-//       const downloadLink = document.createElement('a');
-//       downloadLink.href = editedImageURL;
-//       downloadLink.download = 'edited_image.png';
-//       downloadLink.click();
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <input type="file" accept="image/*" onChange={handleUploadImage} />
-//       <button onClick={handleEraser}>Eraser</button>
-//       <button onClick={undo}>Undo</button>
-//       <button onClick={redo}>Redo</button>
-//       <button onClick={saveImage}>Save</button>
-//       <canvas ref={canvasRef} width={800} height={600} />
-//     </div>
-//   );
-// }
-
-// export default ImageEditor;
-
 import React, { useState, useRef } from 'react';
 
 function ImageEditor() {
@@ -109,6 +6,8 @@ function ImageEditor() {
   const [history, setHistory] = useState([]);
   const [historyIndex, setHistoryIndex] = useState(-1);
   const [image, setImage] = useState(null);
+  const [eraserSize, setEraserSize] = useState(20); // Initial eraser size
+  const originalImageData = useRef(null);
 
   const handleUploadImage = (event) => {
     const file = event.target.files[0];
@@ -123,6 +22,7 @@ function ImageEditor() {
           const newCtx = canvas.getContext('2d');
           newCtx.drawImage(newImage, 0, 0, canvas.width, canvas.height);
           setCtx(newCtx);
+          originalImageData.current = newCtx.getImageData(0, 0, canvas.width, canvas.height);
           setHistory([{ imageData: newCtx.getImageData(0, 0, canvas.width, canvas.height) }]);
           setHistoryIndex(0);
         };
@@ -145,9 +45,9 @@ function ImageEditor() {
     const x = e.clientX - canvasRect.left;
     const y = e.clientY - canvasRect.top;
 
-    ctx.clearRect(x, y, 20, 20);
+    ctx.clearRect(x, y, eraserSize, eraserSize);
     ctx.fillStyle = 'rgba(0, 0, 0, 0)'; // Transparent color
-    ctx.fillRect(x, y, 20, 20);
+    ctx.fillRect(x, y, eraserSize, eraserSize);
   };
 
   const stopErasing = () => {
@@ -156,11 +56,16 @@ function ImageEditor() {
     saveState();
   };
 
+  const handleEraserSizeChange = (event) => {
+    setEraserSize(parseInt(event.target.value));
+  };
+
   const undo = () => {
     if (historyIndex > 0) {
       setHistoryIndex(historyIndex - 1);
       const previousState = history[historyIndex - 1];
       ctx.putImageData(previousState.imageData, 0, 0);
+      updateCanvasSize();
     }
   };
 
@@ -169,6 +74,45 @@ function ImageEditor() {
       setHistoryIndex(historyIndex + 1);
       const nextState = history[historyIndex + 1];
       ctx.putImageData(nextState.imageData, 0, 0);
+      updateCanvasSize();
+    }
+  };
+
+  const updateCanvasSize = () => {
+    const canvas = canvasRef.current;
+    const nonTransparentPixels = findNonTransparentPixels(canvas);
+    if (nonTransparentPixels) {
+      canvas.width = nonTransparentPixels.width;
+      canvas.height = nonTransparentPixels.height;
+      ctx.drawImage(image, 0, 0, nonTransparentPixels.width, nonTransparentPixels.height);
+    }
+  };
+
+  const findNonTransparentPixels = (canvas) => {
+    const ctx = canvas.getContext('2d');
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+
+    let minX = canvas.width;
+    let minY = canvas.height;
+    let maxX = 0;
+    let maxY = 0;
+    for (let y = 0; y < canvas.height; y++) {
+      for (let x = 0; x < canvas.width; x++) {
+        const index = (y * canvas.width + x) * 4;
+        if (data[index + 3] !== 0) {
+          minX = Math.min(minX, x);
+          minY = Math.min(minY, y);
+          maxX = Math.max(maxX, x);
+          maxY = Math.max(maxY, y);
+        }
+      }
+    }
+
+    if (minX <= maxX && minY <= maxY) {
+      return { width: maxX - minX + 1, height: maxY - minY + 1 };
+    } else {
+      return null;
     }
   };
 
@@ -190,26 +134,11 @@ function ImageEditor() {
     }
   };
 
-  const performBackgroundRemoval = () => {
-    if (ctx) {
-      const canvas = canvasRef.current;
-      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-      const data = imageData.data;
-
-      // Simple color-based background removal attempt
-      for (let i = 0; i < data.length; i += 4) {
-        const red = data[i];
-        const green = data[i + 1];
-        const blue = data[i + 2];
-
-        // Adjust these thresholds based on your needs
-        if (red < 200 && green > 200 && blue < 200) {
-          data[i + 3] = 0; // Set alpha channel to 0 (transparent)
-        }
-      }
-
-      ctx.putImageData(imageData, 0, 0);
+  const restoreImage = () => {
+    if (ctx && originalImageData.current) {
+      ctx.putImageData(originalImageData.current, 0, 0);
       saveState();
+      updateCanvasSize();
     }
   };
 
@@ -217,10 +146,17 @@ function ImageEditor() {
     <div>
       <input type="file" accept="image/*" onChange={handleUploadImage} />
       <button onClick={handleEraser}>Eraser</button>
+      <input
+        type="range"
+        min="5"
+        max="100"
+        value={eraserSize}
+        onChange={handleEraserSizeChange}
+      />
       <button onClick={undo}>Undo</button>
       <button onClick={redo}>Redo</button>
       <button onClick={saveImage}>Save</button>
-      <button onClick={performBackgroundRemoval}>Remove Background</button>
+      <button onClick={restoreImage}>Restore Image</button>
       <canvas ref={canvasRef} width={800} height={600} />
     </div>
   );
